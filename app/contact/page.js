02e53e0c10c103b0dda90c8ed3392cb9d0c43edf
Chuan Liu <frankchuan.liu@mail.utoronto.ca>
2024-05-25 02:48:37 -0400

    debugged router

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,13 +1,13 @@
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import HomePage from './components/HomePage';
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import ContactPage from "../components/ContactPage";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-[#121212]">
       <NavBar />
       <div className="container mt-24 mx-auto px-12 flex-grow">
-        <HomePage />
+        <ContactPage />
       </div>
       <Footer />
     </div>
